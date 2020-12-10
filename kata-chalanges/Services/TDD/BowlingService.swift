48027ec054dc505d6f1fb1e3143900e5c08481df
Лев Бондаренко <lion.bond93@gmail.com>
2020-12-10 22:57:21 +0500

    add: bowling test

---
+++ kata-chalanges/Services/TDD/BowlingService.swift
@@ -0,0 +1,21 @@
+//
+//  BowlingService.swift
+//  kata-chalanges
+//
+//  Created by Лев Бондаренко on 10.12.2020.
+//
+
+import Foundation
+
+class BowlingService {
+    var rolls: [Int] = []
+    
+    func roll(pins: [Int]) {
+        rolls + pins
+        Double
+    }
+    
+    func getScore(){
+        
+    }
+}
---
+++ kata-chalangesTests/BowlingTests/BowlingTests.swift
@@ -0,0 +1,16 @@
+//
+//  BowlingTests.swift
+//  kata-chalangesTests
+//
+//  Created by Лев Бондаренко on 10.12.2020.
+//
+
+import Nimble
+import Quick
+@testable import kata_chalanges
+
+class BowlingTest: QuickSpec {
+    override func spec() {
+        <#code#>
+    }
+}
